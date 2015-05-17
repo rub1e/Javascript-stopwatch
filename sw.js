a35c3ba9ced5bda9c8653e4a337218fb4b6a7bf8
rub1e <alex.rubner@gmail.com>
2015-05-17 16:12:41 +0100

    cleaned up and added comments to js functions

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+// Stopwatch with start/stop/split/reset. Helper functions at the bottom for clarity
+
 var T = (function () { 
     'use strict';
 
@@ -17,26 +19,37 @@ var T = (function () {
 
 // Start timer or continue from paused time
 function startTimer() {
+
+	// If the timer has been paused, the time already elapsed is subtracted
+	// from the time NOW so that the elapsed time is maintained when the
+	// timer restarts
 	T.timeStarted = new Date().getTime() - T.timeElapsed; 
+
+	// Need setInterval as a variable so it can be cleared on stop/reset
 	update = setInterval(postTime, 10);
-	document.getElementById("start").disabled = true;
-	document.getElementById("stop").disabled = false;
-	document.getElementById("split").disabled = false;
+
+	//Disable/enable appropriate buttons	
+	document.getElementById("reset").disabled = false;	
+	startButtons();
+	
 	return update;
 }
 
 
-//Freeze the timer at time when button pressed
+
+//Freeze the timer
 function stopTimer(){
+	
+	// Stop the interval updating
 	clearInterval(update);
-	T.timeElapsed = new Date().getTime() - T.timeStarted;
-	document.getElementById("start").disabled = false;
-	document.getElementById("stop").disabled = true;
-	document.getElementById("split").disabled = true;
+
+	//Disable/enable appropriate buttons
+	stopButtons();
 }
 
 
-//Record split without stopping timer
+
+// Record split without stopping timer. Will add lap time at some point
 function split(){
 	T.splitCount++;
 	document.getElementById("splits").innerHTML += "</br>Split " + T.splitCount + ": " + render(T.timeElapsed);
@@ -49,26 +62,34 @@ function reset(){
 	clearInterval(update);
 	T.timeStarted = 0;
 	T.timeElapsed = 0;
-
+	T.splitCount = 0;
 	document.getElementById("timer").innerHTML = "00:00:00";
-	
-	document.getElementById("start").disabled = false;
-	document.getElementById("stop").disabled = true;
-	document.getElementById("split").disabled = true;	
+	document.getElementById("reset").disabled = true;	
 
-	document.getElementById("splits").innerHTML = "Splits"
+	stopButtons()
+
+	document.getElementById("splits").innerHTML = "<br>Splits</br>"
 }
 
-//helper function
-function addLeadingZero (n) {
-    if(n <= 9) {
-    	return '0'+ n; 
-    	} else {
-        return '' + n; 
-    }
-} 
 
-//Post time in mm/ss/cc
+
+// Disable start button, enable stop and split
+function startButtons(){
+	document.getElementById("start").disabled = true;
+	document.getElementById("stop").disabled = false;
+	document.getElementById("split").disabled = false;
+
+}
+
+// Enable start button, disable stop and split
+function stopButtons(){
+	document.getElementById("start").disabled = false;
+	document.getElementById("stop").disabled = true;
+	document.getElementById("split").disabled = true;
+
+}
+
+// Convert time to mm/ss/cc
 function render(){
 	T.timeElapsed = new Date().getTime() - T.timeStarted;
 
@@ -80,10 +101,19 @@ function render(){
     toRender -= secs * 1000;
     var cent = Math.round(toRender/10);
 
-	var renderedTime = addLeadingZero(mins) + ":" + addLeadingZero(secs) + ":" + addLeadingZero(cent); 
-	return renderedTime;
+	return addLeadingZero(mins) + ":" + addLeadingZero(secs) + ":" + addLeadingZero(cent); 
 }
 
+//helper function to make render neater
+function addLeadingZero (n) {
+    if(n <= 9) {
+    	return '0'+ n; 
+    	} else {
+        return '' + n; 
+    }
+} 
+
+// Post the time in the timer div
 function postTime(time) {
 	document.getElementById("timer").innerHTML = render(time);
 
